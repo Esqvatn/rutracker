91070ce80f0ff549c77f50e49e70f70632771784
Esqvatn <favn.hq@gmail.com>
2017-05-09 22:31:28 +0300

    Добавьте файлы проекта.

---
+++ rutracker/MainWindow.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace rutracker
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNewSearch = new System.Windows.Forms.Button();
+            this.LableCountTorrents = new System.Windows.Forms.Label();
+            this.textFieldSearch = new System.Windows.Forms.TextBox();
+            this.LableCountFoundTor = new System.Windows.Forms.Label();
+            this.FoundItemsView = new System.Windows.Forms.DataGridView();
+            this.forumName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.registred_at = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelState = new System.Windows.Forms.Label();
+            this.buttonStopSearch = new System.Windows.Forms.Button();
+            this.buttonContinueSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.FoundItemsView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonNewSearch
+            // 
+            this.buttonNewSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNewSearch.Location = new System.Drawing.Point(486, 12);
+            this.buttonNewSearch.Name = "buttonNewSearch";
+            this.buttonNewSearch.Size = new System.Drawing.Size(100, 20);
+            this.buttonNewSearch.TabIndex = 0;
+            this.buttonNewSearch.Text = "Новый поиск";
+            this.buttonNewSearch.UseVisualStyleBackColor = true;
+            this.buttonNewSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonNewSearch_Click);
+            // 
+            // LableCountTorrents
+            // 
+            this.LableCountTorrents.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LableCountTorrents.AutoSize = true;
+            this.LableCountTorrents.Location = new System.Drawing.Point(302, 444);
+            this.LableCountTorrents.Name = "LableCountTorrents";
+            this.LableCountTorrents.Size = new System.Drawing.Size(127, 13);
+            this.LableCountTorrents.TabIndex = 1;
+            this.LableCountTorrents.Text = "Количество торрентов: ";
+            // 
+            // textFieldSearch
+            // 
+            this.textFieldSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textFieldSearch.Location = new System.Drawing.Point(12, 12);
+            this.textFieldSearch.Name = "textFieldSearch";
+            this.textFieldSearch.Size = new System.Drawing.Size(468, 20);
+            this.textFieldSearch.TabIndex = 2;
+            // 
+            // LableCountFoundTor
+            // 
+            this.LableCountFoundTor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LableCountFoundTor.AutoSize = true;
+            this.LableCountFoundTor.Location = new System.Drawing.Point(521, 444);
+            this.LableCountFoundTor.Name = "LableCountFoundTor";
+            this.LableCountFoundTor.Size = new System.Drawing.Size(179, 13);
+            this.LableCountFoundTor.TabIndex = 4;
+            this.LableCountFoundTor.Text = "Количество найденых торрентов: ";
+            // 
+            // FoundItemsView
+            // 
+            this.FoundItemsView.AllowUserToAddRows = false;
+            this.FoundItemsView.AllowUserToDeleteRows = false;
+            this.FoundItemsView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FoundItemsView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FoundItemsView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.forumName,
+            this.titel,
+            this.size,
+            this.registred_at});
+            this.FoundItemsView.Location = new System.Drawing.Point(15, 39);
+            this.FoundItemsView.Name = "FoundItemsView";
+            this.FoundItemsView.ReadOnly = true;
+            this.FoundItemsView.RowHeadersVisible = false;
+            this.FoundItemsView.ShowCellToolTips = false;
+            this.FoundItemsView.ShowEditingIcon = false;
+            this.FoundItemsView.ShowRowErrors = false;
+            this.FoundItemsView.Size = new System.Drawing.Size(737, 399);
+            this.FoundItemsView.TabIndex = 5;
+            // 
+            // forumName
+            // 
+            this.forumName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.forumName.HeaderText = "Форум";
+            this.forumName.Name = "forumName";
+            this.forumName.ReadOnly = true;
+            this.forumName.Width = 68;
+            // 
+            // titel
+            // 
+            this.titel.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.titel.HeaderText = "Тема";
+            this.titel.Name = "titel";
+            this.titel.ReadOnly = true;
+            // 
+            // size
+            // 
+            this.size.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.size.HeaderText = "Размер";
+            this.size.Name = "size";
+            this.size.ReadOnly = true;
+            this.size.Width = 71;
+            // 
+            // registred_at
+            // 
+            this.registred_at.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.registred_at.HeaderText = "Дата регистрации";
+            this.registred_at.Name = "registred_at";
+            this.registred_at.ReadOnly = true;
+            this.registred_at.Width = 114;
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.Location = new System.Drawing.Point(15, 443);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(67, 13);
+            this.labelState.TabIndex = 6;
+            this.labelState.Text = "Состояние: ";
+            // 
+            // buttonStopSearch
+            // 
+            this.buttonStopSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStopSearch.Location = new System.Drawing.Point(592, 12);
+            this.buttonStopSearch.Name = "buttonStopSearch";
+            this.buttonStopSearch.Size = new System.Drawing.Size(66, 20);
+            this.buttonStopSearch.TabIndex = 7;
+            this.buttonStopSearch.Text = "Стоп";
+            this.buttonStopSearch.UseVisualStyleBackColor = true;
+            this.buttonStopSearch.Click += new System.EventHandler(this.buttonStopSearch_Click);
+            // 
+            // buttonContinueSearch
+            // 
+            this.buttonContinueSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonContinueSearch.Location = new System.Drawing.Point(664, 12);
+            this.buttonContinueSearch.Name = "buttonContinueSearch";
+            this.buttonContinueSearch.Size = new System.Drawing.Size(84, 20);
+            this.buttonContinueSearch.TabIndex = 8;
+            this.buttonContinueSearch.Text = "Продолжить";
+            this.buttonContinueSearch.UseVisualStyleBackColor = true;
+            this.buttonContinueSearch.Click += new System.EventHandler(this.buttonContinueSearch_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 466);
+            this.Controls.Add(this.buttonContinueSearch);
+            this.Controls.Add(this.buttonStopSearch);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.FoundItemsView);
+            this.Controls.Add(this.LableCountFoundTor);
+            this.Controls.Add(this.buttonNewSearch);
+            this.Controls.Add(this.LableCountTorrents);
+            this.Controls.Add(this.textFieldSearch);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.FoundItemsView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNewSearch;
+        private System.Windows.Forms.Label LableCountTorrents;
+        private System.Windows.Forms.TextBox textFieldSearch;
+        private System.Windows.Forms.Label LableCountFoundTor;
+        private System.Windows.Forms.DataGridView FoundItemsView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn forumName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn registred_at;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Button buttonStopSearch;
+        private System.Windows.Forms.Button buttonContinueSearch;
+    }
+}
+
---
+++ rutracker/MainWindow.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Xml;
+
+using System.Threading;
+
+namespace rutracker
+{
+    public partial class MainWindow : Form
+    {
+        XmlReader reader;
+        Size oldSize;
+        List<structTorrent> foundTor;
+        bool continueSearch, eof, endCyrcle;
+        int countTorrents = 0, countTorrentsFound = 0;
+        string path, stateProg = "Состояние: ";
+
+        Thread threadSearch;
+        delegate void delegateAddRow(structTorrent _st);
+        delegate void delegateLableText(structTorrent _st);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonNewSearch.Enabled = true;
+            buttonStopSearch.Enabled = false;
+            buttonContinueSearch.Enabled = false;
+
+            //path = "C:\\Users\\Dev\\Documents\\backup.20170208185701.xml";
+
+            path = ".\\backup.20170208185701.xml";
+            if (!File.Exists(path))
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = "c:\\";
+                ofd.Filter = "xml files (*.xml)|*xml";
+                ofd.RestoreDirectory = true;
+
+                ofd.ShowDialog();
+                path = ofd.FileName;
+            }
+            oldSize = this.Size;
+        }
+
+        private void buttonStopSearch_Click(object sender, EventArgs e)
+        {
+            if (threadSearch.IsAlive)
+            {
+                string strCT = "Количество торрентов: ", strCFT = "Количество найденых торрентов: ";
+                continueSearch = false;
+                labelState.Text = stateProg + " остановка поиска";
+                while (threadSearch.IsAlive) ;
+                threadSearch.Abort();
+                threadSearch = new Thread(Search);
+                LableCountTorrents.Text = strCT + countTorrents.ToString();
+                LableCountFoundTor.Text = strCFT + countTorrentsFound.ToString();
+                labelState.Text = stateProg + " поиск остановлен";
+                buttonNewSearch.Enabled = true;
+                buttonStopSearch.Enabled = false;
+                buttonContinueSearch.Enabled = true;
+            }
+        }
+
+        private void buttonNewSearch_Click(object sender, MouseEventArgs e)
+        {
+            if (textFieldSearch.Text.Length > 2)
+            {
+                reader = XmlReader.Create(path);
+                foundTor = new List<structTorrent>();
+                FoundItemsView.Rows.Clear();
+
+                threadSearch = new Thread(Search);
+                continueSearch = true;
+                if (threadSearch.ThreadState == ThreadState.Unstarted)
+                {
+                    threadSearch.Start();
+                    labelState.Text = stateProg + " идет поиск";
+                    buttonNewSearch.Enabled = false;
+                    buttonStopSearch.Enabled = true;
+                    buttonContinueSearch.Enabled = false;
+                }
+            }
+            else
+                MessageBox.Show("Введите тектс для поиска");
+        }
+
+        private void buttonContinueSearch_Click(object sender, EventArgs e)
+        {
+            threadSearch = new Thread(Search);
+            continueSearch = true;
+            if (threadSearch.ThreadState == ThreadState.Unstarted)
+            {
+                threadSearch.Start();
+                labelState.Text = stateProg + " поиск продолжен";
+                buttonNewSearch.Enabled = false;
+                buttonStopSearch.Enabled = true;
+                buttonContinueSearch.Enabled = false;
+            }
+        }
+        /*
+        private void SearchButtonClick(object sender, MouseEventArgs e)
+        {
+            if (threadSearch == null)
+            {
+                threadSearch = new Thread(Search);
+            }
+            if (threadSearch.IsAlive)
+            {
+                string strCT = "Количество торрентов: ", strCFT = "Количество найденых торрентов: ";
+                continueSearch = false;
+                this.buttonNewSearch.Text = "Поиск";
+                labelState.Text = stateProg + " остановка поиска";
+                while (threadSearch.IsAlive);
+                threadSearch.Abort();
+                threadSearch = new Thread(Search);
+                LableCountTorrents.Text = strCT + countTorrents.ToString();
+                LableCountFoundTor.Text = strCFT + countTorrentsFound.ToString();
+                labelState.Text = stateProg + " поиск остановлен";
+            }
+            else
+            {
+                if (reader.EOF)
+                {
+                    reader = XmlReader.Create(path);
+                    foundTor = new List<structTorrent>();
+                    countTorrents = 0;
+                    countTorrentsFound = 0;
+                    FoundItemsView.Rows.Clear();
+                }
+                continueSearch = true;
+                if(threadSearch.ThreadState == ThreadState.Unstarted)
+                {
+                    this.buttonNewSearch.Text = "Стоп";
+                    threadSearch.Start();
+                    labelState.Text = stateProg + " идет поиск";
+                }
+            }
+        }
+        */
+        private void Search()
+        {
+            string[] SearchParts = textFieldSearch.Text.Split(' ');
+
+            structTorrent temp = new structTorrent();
+
+            string nameElement = "";
+            bool suitable = false;
+
+            endCyrcle = false;
+            while (reader.Read() && continueSearch)
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        switch (reader.Name)
+                        {
+                            case "torrent":
+                                nameElement = reader.Name;
+                                temp.idTopic = Int32.Parse(reader.GetAttribute("id"));
+                                temp.registred_at = DateTime.Parse(reader.GetAttribute("registred_at"));
+                                temp.setSize(Int64.Parse(reader.GetAttribute("size")));
+                                temp.magnetURL = "";
+                                temp.content = "";
+                                countTorrents++;
+                                break;
+                            case "title":
+                                nameElement = reader.Name;
+                                break;
+                            case "magnet":
+                                nameElement = reader.Name;
+                                break;
+                            case "forum":
+                                nameElement = reader.Name;
+                                temp.idForum = Int32.Parse(reader.GetAttribute("id"));
+                                break;
+                            case "content":
+                                nameElement = reader.Name;
+                                break;
+                        }
+                        break;
+                    case XmlNodeType.CDATA:
+                        switch (nameElement)
+                        {
+                            case "title":
+                                if (searchInCDATA(SearchParts, reader.Value))
+                                {
+                                    temp.titel = reader.Value;
+                                    suitable = true;
+                                    countTorrentsFound++;
+                                }
+                                break;
+                            case "magnet":
+                                if (suitable)
+                                {
+                                    temp.magnetURL = reader.Value;
+                                }
+                                break;
+                            case "forum":
+                                if (suitable)
+                                {
+                                    temp.forumName = reader.Value;
+                                }
+                                break;
+                            case "content":
+                                if (suitable)
+                                {
+                                    temp.content = reader.Value;
+                                }
+                                break;
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        if (reader.Name == "torrent")
+                        {
+                            if (suitable)
+                            {
+                                foundTor.Add(temp);
+                                if (FoundItemsView.InvokeRequired)
+                                {
+                                    delegateAddRow dAddRow = delegate (structTorrent st)
+                                    {
+                                        string[] str = new string[] { st.forumName, st.titel, st.getSize(), st.registred_at.ToShortDateString() };
+                                        FoundItemsView.Rows.Add(str);
+                                    };
+                                    FoundItemsView.Invoke(dAddRow, temp);
+                                }
+                                else
+                                {
+                                    string[] str = new string[] { temp.forumName, temp.titel, temp.getSize(), temp.registred_at.ToShortDateString() };
+                                    FoundItemsView.Rows.Add(str);
+                                }
+                            }
+                            suitable = false;
+                        }
+                        break;
+                }
+            }
+            if (continueSearch)
+                MessageBox.Show("Поиск окончен");
+            else
+                MessageBox.Show("Поиск остановлен");
+        }
+
+        private bool searchInCDATA(string[] found, string inStr)
+        {
+            string str = inStr.ToLower();
+            int countFound = 0;
+            for (int i = 0; i < found.Length; i++)
+                if (str.Contains(found[i].ToLower()))
+                    countFound++;
+            if (countFound == found.Length)
+                return true;
+            return false;
+        }
+
+        public struct structTorrent
+        {
+            public int idTopic, idForum;
+            public DateTime registred_at;
+            public Int64 sizeByts;
+            short size;
+            byte sizeUnit;
+            string[] Units;
+            public string titel,magnetURL,content,forumName;
+
+            public void setSize(Int64 _size)
+            {
+                creataUnits();
+                sizeUnit = 0;
+                sizeByts = _size;
+                Int64 _sizeT = _size;
+                while (_sizeT > 1024)
+                {
+                    _sizeT /= 1024;
+                    sizeUnit++;
+                }
+                size = (short)(_sizeT%short.MaxValue);
+            }
+            public string getSize()
+            {
+                if (sizeUnit < Units.Length)
+                    return size.ToString() + " " + Units[sizeUnit];
+                return size.ToString() + " " + Units[Units.Length - 1];
+            }
+            public void creataUnits()
+            {
+                Units = new string[] {"Б", "Кбайт", "Мбайт", "Гбайт", "Тбайт"};
+            }
+        }
+    }
+}
